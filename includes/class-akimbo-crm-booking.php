c62545ce68f79f27f7d71abd4a2cefcab5291bcb
Claire Reincastle <clairemr@hotmail.com>
2020-08-19 11:06:27 +1000

    fixed order matching

--- includes/class-akimbo-crm-booking.php
+++ includes/class-akimbo-crm-booking.php
@@ -18,7 +18,8 @@ class Akimbo_Crm_Booking{
 		global $wpdb;
         $this->booking_id = $booking_id;
         $this->booking_info = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}crm_class_list WHERE list_id = '$booking_id'");
-        $order_id = $this->booking_info->class_id;
+		$this->class_id = $this->booking_info->class_id;
+		$order_id = $this->booking_info->class_id;
         if($order_id <= 1){
 			$order = $this->match_order();
 			if(is_array($order)){//multiple orders returned
@@ -27,18 +28,24 @@ class Akimbo_Crm_Booking{
 		}else{
 			$order = wc_get_order( $order_id );
 		}
-		$this->order = $order;
-		$this->order_id = $order->get_id();
-		if($this->order_id >= 1){
+		
+		if($order){
+			$this->order = $order;
+			$this->order_id = $order->get_id();
 			$this->items = $order->get_items();
 			$this->customer = $order->get_user();
-			
+			if($this->class_id <=1){//update order_id
+				$table = $wpdb->prefix.'crm_class_list';
+				$data = array("class_id" => $this->order_id);
+				$where = array("list_id" => $this->booking_info->list_id);
+				$result = $wpdb->update( $table, $data, $where);
+				if($result){$this->class_id = $this->order_id;}
+			}
 			foreach ( $this->items as $item_id => $item_data ){
 				if($item_data['book_date']){
 					$this->booking_info->book_date = $item_data['book_date'];
 					$this->booking_info->product_id = $item_data['product_id'];
 					$this->booking_info->variation = $item_data['name'];
-					//$this->variation = $item_data['name'];
 				}
 				$this->booking_info->guest_of_honour = ($item_data['birthday_name']) ? $item_data['birthday_name'] : NULL;
 			}
@@ -54,7 +61,12 @@ class Akimbo_Crm_Booking{
 	}
 
 	function match_order(){
-		$matched_orders = crm_match_booking_orders($this->booking_info->session_date);
+		if($this->class_id >= 1){
+			$matched_orders = array(wc_get_order( $this->class_id ));
+		}else{
+			$matched_orders = crm_match_booking_orders($this->booking_info->session_date);
+		}
+		
 		return $matched_orders;
 	}
 
